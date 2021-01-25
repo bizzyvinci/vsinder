f1f3fc1a516a936f9e4f13ba1bbd8ffba4455e20
pizzy <horlasehinde@gmail.com>
2021-01-26 00:32:24 +0100

    Add two code pics

---
+++ f__k_me.py
@@ -0,0 +1,6 @@
+preference = input("Will you like to f__k me ;-)\v")
+
+if preference.lower() in ("yes", "y", "1"):
+	print("Swipe right and go to https://github.com/bizzyvinci/vsinder")
+else:
+	print("Swipe right")
